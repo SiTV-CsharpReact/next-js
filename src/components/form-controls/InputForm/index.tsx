3fc30b76133f4cfb86911b42b14eff1d28f48ee1
Si Ta Van <sitv@fpts.com.vn>
2023-02-15 17:36:06 +0700

    update_new

---
+++ src/components/form-controls/InputForm/index.tsx
@@ -0,0 +1,13 @@
+
+import { TextField } from '@mui/material'
+import PropTypes from 'prop-types'
+InputField.propTypes ={
+
+}
+function InputField(props:any){
+    return (
+        <div>
+            <TextField fullWidth></TextField>
+        </div>
+    )
+}

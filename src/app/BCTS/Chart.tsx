1f9fc23b123d1e55651175c28dd7098f28d98aea
Si Ta Van <sitv@fpts.com.vn>
2023-02-28 16:33:54 +0700

    update_new

--- src/app/BCTS/Chart.tsx
+++ src/app/BCTS/Chart.tsx
@@ -1,111 +1,120 @@
-"use client"
-import React, { useState, useEffect } from 'react';
-import { Bar } from 'react-chartjs-2';
-import {
-  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
-  BarElement,
-  Title,
-  Tooltip,
-  Legend,
-} from 'chart.js';
-import Box from '@mui/material/Box/Box';
-import { FormControl, InputLabel, MenuItem, Paper, Select, SelectChangeEvent, Typography, useTheme } from '@mui/material';
+// "use client"
+// import React, { useState, useEffect } from 'react';
+// import { Bar } from 'react-chartjs-2';
+// import {
+//   Chart as ChartJS,
+//   CategoryScale,
+//   LinearScale,
+//   BarElement,
+//   Title,
+//   Tooltip,
+//   Legend,
+// } from 'chart.js';
+// import Box from '@mui/material/Box/Box';
+// import { FormControl, InputLabel, MenuItem, Paper, Select, SelectChangeEvent, Typography, useTheme } from '@mui/material';
 
-ChartJS.register(
-  CategoryScale,
-  LinearScale,
-  BarElement,
-  Title,
-  Tooltip,
-  Legend
-);
+// ChartJS.register(
+//   CategoryScale,
+//   LinearScale,
+//   BarElement,
+//   Title,
+//   Tooltip,
+//   Legend
+// );
 
 
 
-const BarChart = (dataChart:any) => {
-  const theme=useTheme();
-    // const [day, setDay] = React.useState('');
+// const BarChart = (dataChart:any) => {
+//   const theme=useTheme();
+//     // const [day, setDay] = React.useState('');
 
-    // const handleChange = (event: SelectChangeEvent) => {
-    //     setDay(event.target.value as string);
+//     // const handleChange = (event: SelectChangeEvent) => {
+//     //     setDay(event.target.value as string);
       
-    // };
-    console.log(dataChart)
-  const [chartData, setChartData] = useState({
-    datasets: [],
-  });
+//     // };
+//     console.log(dataChart)
+//   const [chartData, setChartData] = useState({
+//     datasets: [],
+//   });
 
-  const [chartOptions, setChartOptions] = useState({});
+//   const [chartOptions, setChartOptions] = useState({});
 
-  useEffect(() => {
-    setChartData({
-        labels: ['Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat', 'Sun'],
-        datasets: [
-            {
-                label: 'NAV',
-                data: [18127, 22201, 19490, 17938, 24182, 17842, 22475],
-                borderColor: '#fff',
-                backgroundColor: '#70AD47',
-                order: 2
-              }
+//   useEffect(() => {
+//     setChartData({
+//         labels: ['Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat', 'Sun'],
+//         datasets: [
+//             {
+//                 label: 'NAV',
+//                 data: [18127, 22201, 19490, 17938, 24182, 17842, 22475],
+//                 borderColor: '#fff',
+//                 backgroundColor: '#70AD47',
+//                 order: 2
+//               }
               
-        ]
-    })
-    setChartOptions({
-        plugins: {
-            colors:{
-                backgroundColor: theme.palette.mode === 'dark' ? '#323232' :'#ececec'
-             },
-            legend: {
-                position: 'top',
-            },
-            title: {
-                display: false,
-                text: 'THÔNG KÊ GIÁ TRỊ TÀI SẢN RÒNG'
-            }
-        },
-        maintainAspectRatio: false,
-        responsive: true
-    })
-  }, [])
+//         ]
+//     })
+//     setChartOptions({
+//         plugins: {
+//             colors:{
+//                 backgroundColor: theme.palette.mode === 'dark' ? '#323232' :'#ececec'
+//              },
+//             legend: {
+//                 position: 'top',
+//             },
+//             title: {
+//                 display: false,
+//                 text: 'THÔNG KÊ GIÁ TRỊ TÀI SẢN RÒNG'
+//             }
+//         },
+//         maintainAspectRatio: false,
+//         responsive: true
+//     })
+//   }, [])
 
-  return (
-    <>
-      <Paper  sx={{bgcolor:theme.palette.mode === 'dark' ? '#323232' :'#ececec',borderRadius:2,height:400}}>
-        <Box display="flex" justifyContent="space-between" paddingTop="10px">
-        <Box></Box>
-        <Typography component="span" textAlign='center'>
-        THÔNG KÊ GIÁ TRỊ TÀI SẢN RÒNG    
-        </Typography>
-        <Box sx={{ minWidth: 100 }}>
-      <FormControl fullWidth style={{height:30}}>
-        <InputLabel id="demo-simple-select-label">Thời gian</InputLabel>
-        <Select
-        defaultValue="20"
-          labelId="demo-simple-select-label"
-          id="demo-simple-select"
+//   return (
+//     <>
+//       <Paper  sx={{bgcolor:theme.palette.mode === 'dark' ? '#323232' :'#ececec',borderRadius:2,height:400}}>
+//         <Box display="flex" justifyContent="space-between" paddingTop="10px">
+//         <Box></Box>
+//         <Typography component="span" textAlign='center'>
+//         THÔNG KÊ GIÁ TRỊ TÀI SẢN RÒNG    
+//         </Typography>
+//         <Box sx={{ minWidth: 100 }}>
+//       <FormControl fullWidth style={{height:30}}>
+//         <InputLabel id="demo-simple-select-label">Thời gian</InputLabel>
+//         <Select
+//         defaultValue="20"
+//           labelId="demo-simple-select-label"
+//           id="demo-simple-select"
          
-          label="Day"
-       /* A function that is called when the value of the select changes. */
-          // onChange={handleChange}
-          sx={{height:30}}
-        >
-          <MenuItem value="20">20 ngày</MenuItem>
-          <MenuItem value="90">90 ngày</MenuItem>
+//           label="Day"
+//        /* A function that is called when the value of the select changes. */
+//           // onChange={handleChange}
+//           sx={{height:30}}
+//         >
+//           <MenuItem value="20">20 ngày</MenuItem>
+//           <MenuItem value="90">90 ngày</MenuItem>
            
-        </Select>
-      </FormControl>
-    </Box>
-        </Box>
-       <Box height="300px">
-       <Bar data={chartData} options={chartOptions} />
-       </Box>
+//         </Select>
+//       </FormControl>
+//     </Box>
+//         </Box>
+//        <Box height="300px">
+//        <Bar data={chartData} options={chartOptions} />
+//        </Box>
        
-      </Paper>
-    </>
-  );
-};
+//       </Paper>
+//     </>
+//   );
+// };
 
-export default BarChart;
+// export default BarChart;
+import React from 'react'
+
+const BarChart = () => {
+  return (
+    <div>BarChart</div>
+  )
+}
+
+export default BarChart
